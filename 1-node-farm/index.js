ef7cba86328767c8b24b2160f06bf96b5993c06e
CodeXploreRePo <codexplore.channel@gmail.com>
2020-10-06 23:29:19 +0800

    Finished to integrate the Overview HTML file with the first API

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -4,8 +4,40 @@ const fs = require("fs");
 
 /////////////////////////////////
 // SERVER
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  "utf-8",
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/card.html`,
+  "utf-8",
+);
+
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/product.html`,
+  "utf-8",
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const replaceTemplate = (temp, product) => {
+  ///{%PRODUCTNAME%}/g => g means global to replace all match REGEX
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  //If Product is NOT Organic
+  //Replacing the placeholder /{%NOT_ORGANIC%}/ with Class Name: "not-organic"
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  return output;
+};
 
 const server = http.createServer((req, res) => {
   //const { query, pathname } = url.parse(req.url, true);
@@ -15,7 +47,15 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
       "Content-type": "text/html",
     });
-    res.end("This is the OVERVIEW");
+
+    //Replace the Place Holders in Card HTML with data in dataObj
+    const cardsHTML = dataObj.map((el) => replaceTemplate(tempCard, el)).join(
+      "",
+    );
+    //Replace "{PRODUCT_CARDS}" with cardsHTML template
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHTML);
+
+    res.end(output);
   } else if (pathname === "/product") {
     res.end("This is the PRODUCT");
   } else if (pathname === "/api") {
@@ -33,6 +73,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
+server.listen(8000, "localhost", () => {
   console.log("Listening to requests on port 8000");
 });
